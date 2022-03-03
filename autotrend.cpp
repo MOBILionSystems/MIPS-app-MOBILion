49b813fd45fb21f6fb5fccfcaffb791455e7d32d
Ding Zhang <ding.zhang@internal.mobilionsystems.com>
2022-03-03 14:29:37 -0500

    Implemented a basic AutoTrend for dC

--- autotrend.cpp
+++ autotrend.cpp
@@ -1,11 +1,17 @@
 #include "autotrend.h"
 #include "ui_autotrend.h"
 #include <QDebug>
+#include <QQueue>
+#include <QPair>
+#include <QFinalState>
+#include <QTimer>
 
 AutoTrend::AutoTrend(Ui::MIPS *w, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::AutoTrend)
 {
+    trendSM = new QStateMachine(this);
+    buildTrendSM();
     mipsui = w;
     ui->setupUi(this);
     _broker = new Broker(this);
@@ -15,12 +21,22 @@ AutoTrend::AutoTrend(Ui::MIPS *w, QWidget *parent) :
     rightValueModel = new QStringListModel(this);
     mathOperatorsModel = new QStringListModel(this);
 
+    electrodeChannelHash.insert("FunnelIn", "leSDCB_1");
+    electrodeChannelHash.insert("FunnelOut", "leSDCB_2");
+    electrodeChannelHash.insert("FunnelCL", "leSDCB_3");
+    electrodeChannelHash.insert("SLIMBias", "leSDCB_4");
+    electrodeChannelHash.insert("ExitCL", "leSDCB_5");
+    electrodeChannelHash.insert("QuadBias", "leSDCB_6");
+
     initUI();
 }
 
 AutoTrend::~AutoTrend()
 {
     delete ui;
+    if(trendSM->isRunning()){
+        qWarning() << "Trend state machine is still running when AutoTrend is destroied.";
+    }
 }
 
 void AutoTrend::on_initDigitizerButton_clicked()
@@ -86,12 +102,133 @@ void AutoTrend::initUI()
     ui->trendStepDuration->setValidator(new QIntValidator(0, 10000, this));
 }
 
-
-void AutoTrend::on_pushButton_6_clicked()
+void AutoTrend::updateDCBias(QString name, double value)
 {
-    QLineEdit *leDCB = mipsui->gbDCbias1->findChild<QLineEdit *>("leSDCB_1");
-    leDCB->setText(QString::number(1234));
+    QLineEdit *leDCB = mipsui->gbDCbias1->findChild<QLineEdit *>(name); // name "leSDCB_1"
+    leDCB->setText(QString::number(value)); // value 1234
     leDCB->setModified(true);
     emit leDCB->editingFinished();
+}
+
+bool AutoTrend::applyRelations(QString startWith, double startValue)
+{
+    int appliedRelations = 0;
+    int totalRelationsInList = relationList.size();
+    QString currentName;
+    double currentValue = 0;
+    QQueue<QPair<QString, double>> calQueue;
+    calQueue.enqueue(QPair<QString, double>(startWith, startValue));
+    while(calQueue.size() > 0){
+        QPair<QString, double> current = calQueue.dequeue();
+        currentName = current.first;
+        currentValue = current.second;
+        for(auto& current : relationList){
+            QStringList mathList = current.split("=", Qt::SkipEmptyParts);
+            if(mathList.size() != 2) continue;
+            QString leftString = mathList.at(0);
+            QString rightString = mathList.at(1);
+            if(rightString.contains(currentName)){ // Assume NameX=NameY+3
+                qDebug() << "applying " << current;
+                if(rightString.contains('+')){
+                    QStringList rightList = rightString.split('+');
+                    if(rightList.size() == 2){
+                        currentValue += rightList.at(1).toDouble();
+                    }
+                }else if(rightString.contains('-')){
+                    QStringList rightList = rightString.split('-');
+                    if(rightList.size() == 2){
+                        currentValue -= rightList.at(1).toDouble();
+                    }
+                }else if(rightString.contains('*')){
+                    QStringList rightList = rightString.split('-');
+                    if(rightList.size() == 2){
+                        currentValue *= rightList.at(1).toDouble();
+                    }
+                }
+
+                if(electrodeChannelHash.contains(leftString))
+                    updateDCBias(electrodeChannelHash.value(leftString), currentValue);
+
+                calQueue.enqueue(QPair<QString, double>(leftString, currentValue));
+                appliedRelations++;
+                if(appliedRelations > totalRelationsInList){
+                    qWarning() << "Loop detected in Relations";
+                    return false;
+                }
+            }
+        }
+    }
+    if(appliedRelations < totalRelationsInList){
+        qWarning() << "Some Relations were not applied";
+        return true;
+    }
+    return true;
+}
+
+void AutoTrend::buildTrendSM()
+{
+    QState* initState = new QState(trendSM);
+    QState* updateTrendState = new QState(trendSM);
+    QState* applyRelationState = new QState(trendSM);
+    QState* waitBeforeAcqState = new QState(trendSM);
+    QState* startAcqState = new QState(trendSM);
+    QState* waitDuringAcqState = new QState(trendSM);
+    QState* stopAcqState = new QState(trendSM);
+    QState* nextStepState = new QState(trendSM);
+    QFinalState* finishState = new QFinalState(trendSM);
+
+    trendSM->setInitialState(initState);
+    connect(initState, &QState::entered, this, [=](){
+        trendName = ui->trendComboBox->currentText();
+        trendFrom = ui->trendFrom->text().toInt();
+        trendTo = ui->trendTo->text().toInt();
+        trendStepSize = ui->trendStepSize->text().toInt();
+        stepDuration = ui->trendStepDuration->text().toInt();
+        currentStep = trendFrom;
+        relationEnabled = ui->relationRatioButton->isChecked();
+        emit nextState();
+    });
+    initState->addTransition(this, &AutoTrend::nextState, updateTrendState);
+
+    connect(updateTrendState, &QState::entered, this, [=](){
+        if(electrodeChannelHash.contains(trendName))
+            updateDCBias(electrodeChannelHash.value(trendName), currentStep);
+
+        emit nextState();
+    });
+    updateTrendState->addTransition(this, &AutoTrend::nextState, applyRelationState);
+
+    connect(applyRelationState, &QState::entered, this, [=](){if(relationEnabled) applyRelations(trendName, currentStep); emit nextState();});
+    applyRelationState->addTransition(this, &AutoTrend::nextState, waitBeforeAcqState);
+
+    connect(waitBeforeAcqState, &QState::entered, this, [=](){QTimer::singleShot(2000, this, [=](){emit nextState();});});
+    waitBeforeAcqState->addTransition(this, &AutoTrend::nextState, startAcqState);
+
+    connect(startAcqState, &QState::entered, this, [=](){_broker->startAcquire(); emit nextState();});
+    startAcqState->addTransition(this, &AutoTrend::nextState, waitDuringAcqState);
+
+    connect(waitDuringAcqState, &QState::entered, this, [=](){QTimer::singleShot(stepDuration, this, [=](){emit nextState();});});
+    waitDuringAcqState->addTransition(this, &AutoTrend::nextState, stopAcqState);
+
+    connect(stopAcqState, &QState::entered, this, [=](){_broker->stopAcquire(); emit nextState();});
+    stopAcqState->addTransition(this, &AutoTrend::nextState, nextStepState);
+
+    connect(nextStepState, &QState::entered, this, [=](){currentStep += trendStepSize; currentStep <= trendTo ? emit nextState() : emit doneAllStates();});
+    nextStepState->addTransition(this, &AutoTrend::nextState, updateTrendState);
+    nextStepState->addTransition(this, &AutoTrend::doneAllStates, finishState);
+
+    connect(trendSM, &QStateMachine::finished, this, [](){qDebug() << "Trend State Machine finished";});
+}
+
+
+void AutoTrend::on_runTrendButton_clicked()
+{
+    trendSM->start();
+}
+
+
+void AutoTrend::on_stopTrendButton_clicked()
+{
+
 }
 
--- autotrend.h
+++ autotrend.h
@@ -3,10 +3,12 @@
 
 #include <QWidget>
 #include <QStringListModel>
+#include <QStateMachine>
 #include "MEyeOn/Broker.h"
 
 #include "ui_mips.h"
 #include "mips.h"
+#include <QHash>
 
 
 namespace Ui {
@@ -21,6 +23,10 @@ public:
     explicit AutoTrend(Ui::MIPS *w, QWidget *parent = nullptr);
     ~AutoTrend();
 
+signals:
+    void nextState();
+    void doneAllStates();
+
 private slots:
     void on_initDigitizerButton_clicked();
 
@@ -32,9 +38,12 @@ private slots:
 
     void on_addRelationButton_clicked();
 
-    void on_pushButton_6_clicked();
+    void on_runTrendButton_clicked();
+
+    void on_stopTrendButton_clicked();
 
 private:
+    QStateMachine* trendSM;
     Ui::MIPS *mipsui;
     Ui::AutoTrend *ui;
     Broker* _broker;
@@ -45,8 +54,21 @@ private:
     QStringList electrodes = {"FunnelIn", "FunnelOut", "FunnelCL", "SLIMBias", "ExitCL", "QuadBias"};
     QStringList mathOperators = {"None", "+", "-", "*"};
     QStringList relationList;
+    QHash<QString, QString> electrodeChannelHash;
+
+
+    QString trendName;
+    int trendFrom = 0;
+    int trendTo = 0;
+    int trendStepSize = 1;
+    int stepDuration = 1;
+    int currentStep = 0;
+    bool relationEnabled = false;
 
     void initUI();
+    void updateDCBias(QString name, double value);
+    bool applyRelations(QString startWith, double startValue);
+    void buildTrendSM();
 };
 
 #endif // AUTOTREND_H
